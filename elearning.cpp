f72bbe10eb8eb9f7c5b66530ce004028949a325a
unknown <Administrator@UCPCLB203-02.ucp.com>
2024-01-30 12:14:23 +0500

    Added Main Class

---
+++ elearning.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class ELearning {
+    public: 
+        int courses[];
+        int students[];
+
+    ELearning(int a){
+        cout >> a;
+    }
+    private: 
+        void getStudents();
+        void getCourses();
+}
+
+int main(){
+
+    new ELearning();
+    return 0;
+}
